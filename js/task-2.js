6d2dcc09e579a3d2b44feefe5464d9b115de08c4
unknown <andrii.hushuvatyi@gmail.com>
2024-08-12 10:44:46 +0300

    three tasks

---
+++ js/task-2.js
@@ -0,0 +1,31 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const indexForRemove = this.#items.indexOf(itemToRemove);
+    indexForRemove > 0 ? this.#items.splice(indexForRemove, 1) : console.log("No items for remove");
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
